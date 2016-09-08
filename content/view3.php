b72a4c2a420c54b1b6d82fab6a8d1e3d42093e3f
Seth Kerr <sethmsk88@yahoo.com>
2016-09-08 17:05:39 -0400

    Create sample of view3 page

---
+++ content/view3.php
@@ -0,0 +1,94 @@
+<?php
+	function printParagraph() {
+		echo 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit mollis felis, eu tincidunt lacus pellentesque et. Nunc in arcu fermentum, efficitur orci non, scelerisque turpis. Vestibulum leo nulla, egestas at vehicula vitae, condimentum eget magna. Sed condimentum sit amet tortor at ultrices. Nullam condimentum, nibh id elementum malesuada, orci diam suscipit ante, vel facilisis felis nisi in nulla. Donec at vestibulum enim. Maecenas odio felis, lacinia vel leo non, euismod ultricies velit. Curabitur fermentum, diam eget ultrices eleifend, odio magna eleifend lectus, nec lacinia ipsum est id tellus. Praesent iaculis tortor nulla, id suscipit elit tincidunt vitae. Ut varius iaculis convallis. Maecenas quis vestibulum justo, eu dictum mi. ';
+	}
+?>
+
+<div class="container">
+	<h4>ROSTER</h4>
+	The Roster narratives for C.S. 3.2.8 are organized as follows:
+
+	<ol>
+		<li><a href="#I">President/Chief Executive Officeer (CEO)</a></li>
+		<li><a href="#II">Vice Presidents</a></li>
+		<li>Sample</li>
+		<li>Sample</li>
+		<li>Sample</li>
+		<li>Sample</li>
+		<li>Sample</li>
+	</ol>
+
+	<a name="I"></a>
+	<h5>I. President/Chief Executive Officer</h5>
+	<p>
+		<u>President/Chief Executive Officer (CEO):</u> Elmira Mangum (Ph.D. in Edication Leadership and Policy)[5]<a name="6">[6]</a> has served as the 
+		<?= printParagraph() ?>
+	</p>
+
+	<a name="II"></a>
+	<h5>II. Vice Presidents</h5>
+	<p>
+		<?= printParagraph() ?>
+	</p>
+	<ol>
+		<li>
+			<u>Provost and Vice President of Academic Affairs:</u>
+			<?= printParagraph() ?>
+			<br><br>
+		</li>
+		<li>
+			<u>The Interim Vice President for Finance and Administration (CFO):</u>
+			Angela Poole (MBA, CPA) has <?= printParagraph() ?>
+			<br><br>
+		</li>
+		<li>
+			<u>Vice President for Student Affairs:</u>
+			<?= printParagraph() ?>
+			<br><br>
+		</li>
+		<li>
+			<u>Vice President for Research</u>
+			<?= printParagraph() ?>
+			<br><br>
+		</li>
+		<li>
+			<u>Vice President for Audit and Compliance</u>
+			<?= printParagraph() ?>
+			<br><br>
+		</li>
+		<li>
+			<u>Vice President for University Advancement</u>
+			<?= printParagraph() ?>
+			<br><br>
+		</li>
+	</ol>
+
+	<h5>II. President's Direct Reports</h5>
+	<p>
+		<?= printParagraph() ?>
+	</p>
+	<p>
+		<?= printParagraph() ?>
+	</p>
+	<p>
+		<?= printParagraph() ?>
+	</p>
+
+
+	<h4>DOCUMENTATION</h4>
+	<h5>3.2.8 Appendix A</h5>
+	<ol>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li><a href="#6">Elmira Mangum, President and CEO</a></li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+	</ol>
+</div>
