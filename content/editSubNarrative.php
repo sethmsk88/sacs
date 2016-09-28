db3bad78e0045263ee632dceae639c2f4e477795
Seth Kerr <sethmsk88@yahoo.com>
2016-09-28 15:36:17 -0400

    Load sections from DB table into editSubNarrative page

--- content/editSubNarrative.php
+++ content/editSubNarrative.php
@@ -1,12 +1,32 @@
 <script src="./js/editSubNarrative.js"></script>
 <link href="./css/editSubNarrative.css" rel="stylesheet">
 
+<?php
+	// Get all sections for this SR
+	$sel_sections = "
+		SELECT id, name, body
+		FROM sacs.section
+		WHERE srid = ?
+	";
+	$stmt = $conn->prepare($sel_sections);
+	$stmt->bind_param("i", $_GET['id']);
+	$stmt->execute();
+	$stmt->store_result();
+	$stmt->bind_result($sid, $sectionName, $body);
+?>
+
 <div class="container">
 	<div class="row">
 		<div class="col-lg-12">
 			<h4>Table of Contents</h4>
+
 			<ol class="begin">
-				<li>President/CEO</li>
+				<?php
+					// output sectionName for each section
+					while ($stmt->fetch()) {
+						echo '<li>' . $sectionName . '</li>';
+					}
+				?>
 			</ol>
 		</div>
 	</div>
@@ -21,15 +41,35 @@
 		<div class="divider"></div>
 	</div>
 
-	<div class="row">
+	<?php
+		// Output each section header and body
+		$stmt->data_seek(0); // rewind result object pointer
+		$sectionNum = 1; // initialize section counter
+		while ($stmt->fetch()) {
+			if ($sectionNum == 1)
+				$numberingClass = "begin";
+			else
+				$numberingClass = "continue";
+	?>
+	<!-- The row class is throwing off the <ol> numbering -->
+	<!-- <div class="row"> -->
 		<!-- Section Header -->
-		<ol class="begin">
-			<li class="h5">President/CEO</li>
-		</ol>
+	<ol class="<?= $numberingClass ?>">
+		<li class="h5"><?= $sectionName ?></li>
+	</ol>
+	<!-- </div> -->
+	<!-- <div class="row"> -->
+	
+	<!-- Section Body -->
+	<div class="section-body">
+		<?= $body ?>
 	</div>
-	<div class="row">
-		<!-- Section Body -->
-	</div>
+	
+	<!-- </div> -->
+	<?php
+			$sectionNum++;
+		}
+	?>
 
 	
 
