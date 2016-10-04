7ae58a4762bb5ee2e1190b9ea65e152f5e63d876
Seth Kerr <sethmsk88@yahoo.com>
2016-10-04 15:42:33 -0400

    Convert admin page to table design

--- content/admin.php
+++ content/admin.php
@@ -8,7 +8,11 @@
 	} else {
 		require_login($loggedIn);
 	}
+?>
 
+<link href="./css/admin.css" rel="stylesheet">
+
+<?php
 	$sel_all_SR = "
 		SELECT id, number, sr_type
 		FROM " . $TABLE_STANDARD_REQUIREMENT . "
@@ -19,24 +23,80 @@
 	$stmt->store_result();
 	$stmt->bind_result($SRID, $number, $sr_type);
 
-	// create arrays to hold different types of SRs
-	$sr_cr = array();
-	$sr_cs = array();
+	// Array to hold SRs
+	$sr_array = array();
 
-	// Sort SRs into their appropriate array
+	// Insert SRs into array
 	while ($stmt->fetch()) {
 		if ($sr_type == 'r')
-			$sr_cr[$SRID] = 'C.R. ' . $number;
+			$sr_array[$SRID] = 'C.R. ' . $number;
 		else if ($sr_type == 's')
-			$sr_cs[$SRID] = 'C.S. ' . $number;
+			$sr_array[$SRID] = 'C.S. ' . $number;
 	}
-
 ?>
 
 <script src="./js/admin.js"></script>
 
 <div class="container">
 
+	<table id="admin-table" class="table">
+		<tr>
+			<th></th>
+			<th>Narrative</th>
+			<th>Supplemental</th>
+			<th>Appendix</th>
+			<th></th>
+		</tr>
+		<?php
+			foreach ($sr_array as $SRID => $sr) {
+		?>
+		<tr>
+			<td><?= $sr ?></td>
+			<td>
+				<div class="btn-group">
+					<button id="view-narrative-<?= $SRID ?>" class="btn btn-primary">View</button>
+					<button id="edit-narrative-<?= $SRID ?>" class="btn btn-warning">Edit</button>
+				</div>
+			</td>
+			<td>
+				<div class="btn-group">
+					<button id="view-supplemental-<?= $SRID ?>" class="btn btn-primary">View</button>
+					<button id="edit-supplemental-<?= $SRID ?>" class="btn btn-warning">Edit</button>
+				</div>
+			</td>
+			<td>
+				<div class="btn-group">
+					<button id="view-appendix-<?= $SRID ?>" class="btn btn-primary">View</button>
+					<button id="edit-appendix-<?= $SRID ?>" class="btn btn-warning">Edit</button>
+				</div>
+			</td>
+			<td>
+				<button id="delete-<?= $SRID ?>" class="btn btn-danger">Delete</button>
+			</td>
+		</tr>
+		<?php
+			}
+		?>
+		<tr>	
+			<td>
+				<button class="btn btn-success" style="width:100%">
+					<span class="glyphicon glyphicon-plus"></span> New
+				</button>
+			</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+
+
+
+
+
+
+	<!-- Old design below -->
+<?php /*
 	<form name="addSR-form" id="addSR-form" action="">
 		<div class="row">
 			<div class="col-lg-4 col-md-5">
@@ -109,4 +169,5 @@
 			</div>
 		</div>
 	</form>
+*/ ?>
 </div>
