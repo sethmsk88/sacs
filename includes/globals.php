cc15b2a6a94515e039c91f7edb7d41d0a3a8195f
Seth Kerr <sethmsk88@yahoo.com>
2016-10-05 14:43:07 -0400

    Make global variables for the new tables

--- includes/globals.php
+++ includes/globals.php
@@ -8,9 +8,15 @@
     if (APP_DEV_MODE == true) {
     	define("TABLE_SECTION", "sacs.dev_section");
         define("TABLE_STANDARD_REQUIREMENT", "sacs.dev_standard_requirement");
+        define("TABLE_APPENDIX_LINK", "sacs.dev_appendix_link");
+        define("TABLE_FILE_UPLOAD", "sacs.dev_file_upload");
+        define("TABLE_APPENDIX_LINK_HAS_FILE_UPLOAD", "sacs.dev_appendix_link_has_file_upload");
+
     } else {
-    	// TABLE_SECTION = "sacs.section";
         define("TABLE_SECTION", "sacs.section");
         define("TABLE_STANDARD_REQUIREMENT", "sacs.standard_requirement");
+        define("TABLE_APPENDIX_LINK", "sacs.appendix_link");
+        define("TABLE_FILE_UPLOAD", "sacs.file_upload");
+        define("TABLE_APPENDIX_LINK_HAS_FILE_UPLOAD", "sacs.appendix_link_has_file_upload");
     }
 ?>
