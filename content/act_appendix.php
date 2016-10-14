d13a593ac9581e45f79abe49e02bd2fc2e3975df
Seth Kerr <sethmsk88@yahoo.com>
2016-10-14 16:43:40 -0400

    Update narrative when ref nums change
    Update the reference numbers in the title/description, narrative, and
    summary for the standard requirement that the references belong to.

--- content/act_appendix.php
+++ content/act_appendix.php
@@ -2,9 +2,19 @@
 	require_once("../includes/globals.php");
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/bootstrap/apps/shared/db_connect.php';
 
-	// get ref num of first link
+	function swapReferences($str, $ref1, $ref2) {
+		$str = str_replace('['. $ref1 .']', '[A]', $str);
+		$str = str_replace('['. $ref2 .']', '[B]', $str);
+		$str = str_replace('[A]', '['. $ref2 .']', $str);
+		$str = str_replace('[B]', '['. $ref1 .']', $str);
+
+		return $str;
+	}
+
+
+	// Get ref num of first link
 	$sel_refNum = "
-		SELECT refNum
+		SELECT refNum, srid
 		FROM " . TABLE_APPENDIX_LINK . "
 		WHERE appendix_link_id = ?
 	";
@@ -16,13 +26,18 @@
 	$result_row = $result->fetch_assoc();
 	$refNum_1 = $result_row['refNum'];
 
-	// get ref num of second link
+
+	// Get ref num of second link
 	$stmt->bind_param("i", $_POST['linkID_2']);
 	$stmt->execute();
 	
 	$result = $stmt->get_result();
 	$result_row = $result->fetch_assoc();
 	$refNum_2 = $result_row['refNum'];
+
+	// Get standard/requirement id for these links
+	$srid = $result_row['srid'];
+
 
 	// Swap rep nums
 	$update_refNum = "
@@ -35,5 +50,35 @@
 	$stmt->execute();
 
 	$stmt->bind_param("ii", $refNum_1, $_POST['linkID_2']);
+	$stmt->execute();
+
+	// Get standard/requirement
+	$sel_sr = "
+		SELECT descr, narrative, summary
+		FROM " . TABLE_STANDARD_REQUIREMENT . "
+		WHERE id = ?
+	";
+	$stmt = $conn->prepare($sel_sr);
+	$stmt->bind_param("i", $srid);
+	$stmt->execute();
+	$stmt->store_result();
+	$stmt->bind_result($descr, $narrative, $summary);
+	$stmt->fetch();
+
+	// Swap all occurrences of ref nums
+	$descr = swapReferences($descr, $refNum_1, $refNum_2);
+	$narrative = swapReferences($narrative, $refNum_1, $refNum_2);
+	$summary = swapReferences($summary, $refNum_1, $refNum_2);
+	
+	// Update standard/requirement
+	$update_sr = "
+		UPDATE " . TABLE_STANDARD_REQUIREMENT . "
+		SET descr = ?,
+			narrative = ?,
+			summary = ?
+		WHERE id = ?
+	";
+	$stmt = $conn->prepare($update_sr);
+	$stmt->bind_param("sssi", $descr, $narrative, $summary, $srid);
 	$stmt->execute();
 ?>
