0c807194ac3d2d7c2df5013fa392d3a586f4448a
Seth Kerr <sethmsk88@yahoo.com>
2016-11-03 14:39:42 -0400

    Add ability to attach file in the edit reference modal

--- includes/editRef_modal.php
+++ includes/editRef_modal.php
@@ -33,16 +33,58 @@
 								class="form-control">
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-lg-12" class="form-group">
-							<label for="refURL">Reference URL</label>
-							<input
-								type="text"
-								name="refURL"
-								id="refURL"
-								class="form-control">
+
+					<div id="box-1" style="display:none;">
+						<div class="row">
+							<div class="col-lg-12" class="form-group">
+								<label for="refURL">Reference URL</label>
+								<input
+									type="text"
+									name="refURL"
+									id="refURL"
+									class="form-control">
+							</div>
+						</div>
+
+						<div class="row" style="margin-top:12px;">
+							<div class="col-lg-12" class="form-group">
+								<button id="attachFile-btn" class="btn btn-primary">Attach File</button>
+							</div>
 						</div>
 					</div>
+
+					<div id="box-2" style="display:none;">
+						<div class="row">
+							<div class="col-lg-12">
+								<label>Attached File:</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-12">
+								<span id="refFile-link"></span>
+								<button id="removeFile-btn" class="btn btn-link btn-lg" title="Remove File" data-fileid="" data-linkid=""><span class="glyphicon glyphicon-remove text-danger"></span></button>
+							</div>
+						</div>
+					</div>
+
+					<div id="box-3" style="display:none;">
+						<div class="row">
+							<div class="col-lg-12" class="form-group">
+								<label for="fileToUpload">Select a file to upload</label>
+								<div class="input-group">
+									<span class="input-group-btn">
+										<span class="btn btn-primary btn-file">
+											Browse <input type="file" name="fileToUpload" id="fileToUpload">
+										</span>
+									</span>
+									<input type="text" class="form-control" readonly="readonly">
+								</div>
+
+							</div>
+						</div>
+					</div>
+
+
 					<input type="hidden" name="linkID" id="linkID" value="">
 				</form>
 			</div>
@@ -66,8 +108,18 @@
 </div>
 
 <script type="text/javascript">
+	var resetModalFields = function() {
+		$('#editRefModal #box-1').hide();
+		$('#editRefModal #box-2').hide();
+		$('#editRefModal #box-3').hide();
+
+		$('#editRefModal input').val("");
+	}
+
 	// Dialog show event handler
 	$('#editRefModal').on('show.bs.modal', function (e) {
+
+		resetModalFields();
 
 		$clickedButton = $(e.relatedTarget);
 
@@ -88,6 +140,30 @@
 				// populate form fields with link info
 				$('#refName').val(response['linkName']);
 				$('#refURL').val(response['linkURL']);
+
+				if (response['isFileRef'] === false) {
+					$('#box-1').show();
+				} else {
+					$('#box-2').show();
+				}
+
+				// If this is a file reference, create a file link
+				if (response.hasOwnProperty('fileName')) {
+					
+					// strip timestamp from filename
+					var modifiedFileName = response['fileName'];
+					var str_arr = modifiedFileName.split('_');
+					str_arr.splice(0,1); // remove timestamp
+					modifiedFileName = str_arr.join('_');
+
+					var refFileLink = '<a href="' + response['linkURL'] + '" target="_blank">' + modifiedFileName + '</a>';
+
+					$('#refFile-link').html(refFileLink);				
+
+					// populate remove button with file_id and link_id
+					$('#removeFile-btn').attr('data-fileid', response['fileID']);
+					$('#removeFile-btn').attr('data-linkid', response['linkID']);
+				}
 			}
 		});
 
@@ -121,6 +197,44 @@
 			}
 		});
 		
+	});
+
+	// Attach File button click handler
+	$('#attachFile-btn').click(function(e) {
+		e.preventDefault();
+
+		$('#box-1').fadeOut(function() {
+			$('#box-3').fadeIn();
+		});
+	});
+
+	// Remove file button click handler
+	$('#removeFile-btn').click(function(e) {
+		e.preventDefault();
+
+		$removeBtn = $(this);
+
+		$.ajax({
+			url: './content/act_appendix.php',
+			type: 'post',
+			data: {
+				'actionType': 3, // remove file action type
+				'fileID': $removeBtn.attr('data-fileid'),
+				'linkID': $removeBtn.attr('data-linkid')
+			},
+			success: function() {
+				// clear the ids from the remove button
+				$removeBtn.attr('data-fileid', '');
+				$removeBtn.attr('data-linkid', '');
+
+				$('#box-2').fadeOut(function() {
+					$('#box-3').fadeIn();
+				});
+			}
+		});
+
+		console.log($(this).attr('data-fileid'));
+		console.log($(this).attr('data-linkid'));
 	});
 
 </script>
