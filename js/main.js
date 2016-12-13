17c69fa9abb43fee9f9251fe4af2721fdf2e05ea
Seth Kerr <sethmsk88@yahoo.com>
2016-12-13 15:42:11 -0500

    Update tinymce config settings

--- js/main.js
+++ js/main.js
@@ -53,34 +53,80 @@ $(document).ready(function (){
 		}
 	});
 
-	var richtext_plugins = 'advlist anchor autolink charmap code contextmenu hr image imagetools link lists paste preview searchreplace spellchecker table textcolor wordcount';
+	var init_tinymce_editor = function(sel, editorHeight) {
+		var richtext_plugins = 'advlist anchor autolink charmap code contextmenu hr image imagetools link lists paste preview searchreplace spellchecker table textcolor wordcount';
 
-	tinymce.init({
-		selector: 'textarea.richtext-lg',
-		height: 350,
-		plugins: richtext_plugins,
-		paste_data_images: true,
-		elementpath: false,
-		convert_urls: false
-	});
+		var menu_config = {
+			file: {title: 'File', items: 'newdocument'},
+			edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall'},
+			insert: {title: 'Insert', items: 'link imageupload | template hr'},
+			view: {title: 'View', items: 'visualaid | preview'},
+			format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
+			table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
+			tools: {title: 'Tools', items: 'spellchecker, code'}
+		};
 
-	tinymce.init({
-		selector: 'textarea.richtext-md',
-		height: 200,
-		plugins: richtext_plugins,
-		paste_data_images: true,
-		elementpath: false,
-		convert_urls: false
-	});
+		var toolbar_config = 'undo redo | bold italic underline | alignleft aligncenter alignright | bullist numlist outdent indent | link imageupload';
 
-	tinymce.init({
-		selector: 'textarea.richtext-sm',
-		height: 80,
-		plugins: richtext_plugins,
-		paste_data_images: true,
-		elementpath: false,
-		convert_urls: false
-	});
+		tinymce.init({
+			selector: sel,
+			height: editorHeight,		
+			plugins: richtext_plugins,
+			menu: menu_config,
+			paste_data_images: true,
+			elementpath: false,
+			convert_urls: false,
+			toolbar: toolbar_config,
+			setup: function(editor) {
+				var inp = $('<input id="tinymce-uploader" type="file" name="pic" accept="image/*" style="display:none">');
+				$(editor.getElement()).parent().append(inp);
+
+				inp.on("change",function(){
+
+					var input = inp.get(0);
+					var file = input.files[0];
+
+					var formData = new FormData();
+					formData.append('fileToUpload', file);
+
+					$.ajax({
+						type: 'POST',
+						url: './content/act_uploadImages.php',
+						data: formData,
+						dataType: 'json',
+						contentType: false,
+						processData: false,
+						success: function(response) {
+							fileURL = response['fileURL'];
+							editor.insertContent('<img src="'+ fileURL +'"/>');
+						}
+					});			
+				});
+
+				editor.addButton('imageupload', {
+					icon: 'mce-ico mce-i-image',
+					title: "Insert image",
+					onclick: function(e) {
+						inp.trigger('click');
+					}
+				});
+
+				editor.addMenuItem('imageupload', {
+					text: 'Insert Image',
+					icon: 'mce-ico mce-i-image',
+					context: 'insert',
+					onclick: function(E) {
+						inp.trigger('click');
+					}
+				});
+			}
+		});
+	}
+
+	init_tinymce_editor('textarea.richtext-lg', 350);
+	init_tinymce_editor('textarea.richtext-md', 200);
+	init_tinymce_editor('textarea.richtext-sm', 80);
+	
 
 	// Prepare overlay for modals
 	$overlay = $('<div id="overlay"></div>');
