2fd689da03f8ae4c78e356e27ffb69616da9b140
Seth Kerr <sethmsk88@yahoo.com>
2017-01-12 17:15:47 -0500

    Create file to export portable version of application

---
+++ content/exportPortable.php
@@ -0,0 +1,8 @@
+<?php
+	require_once "../includes/globals.php";
+	require_once "../includes/functions.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/bootstrap/apps/shared/db_connect.php';
+
+	
+	
+?>
